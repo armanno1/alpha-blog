7731949ee6ca13679df9a8dcbbcbeef8535013dc
Rails Udemy Course <railsudemycourse@gmail.com>
2019-08-22 12:25:52 +0100

    add admin user func

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:edit, :update, :show]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   before_action :require_user, except: [:index, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
 
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.user = current_user
     if @article.save
-      flash[:success] = "Article was successfully created"
+      flash[:success] = "Article was successfully created."
       redirect_to article_path(@article)
     else
       render 'new'
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:success] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated."
       redirect_to article_path(@article)
     else
       render "edit"
@@ -42,11 +42,12 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    flash[:danger] = "Article was successfully destroyed"
+    flash[:danger] = "Article was successfully destroyed."
     redirect_to articles_path
   end
 
   private
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
@@ -56,8 +57,8 @@ class ArticlesController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @article.user
-        flash[:danger] = "only edit your own articles"
+      if current_user != @article.user && !current_user.admin?
+        flash[:danger] = "You can only edit or delete your own articles."
         redirect_to root_path
       end
     end
