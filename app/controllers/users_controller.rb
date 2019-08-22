75d2d7bee8a63f43270f45a07299e7461f0b3fef
Rails Udemy Course <railsudemycourse@gmail.com>
2019-08-22 11:51:25 +0100

    users login at signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -53,4 +54,5 @@ class UsersController < ApplicationController
       flash[:danger] = "you can only edit your own account"
       redirect_to root_path
     end
+  end
 end
