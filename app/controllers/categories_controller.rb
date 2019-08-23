3030af2f6ad4f17cabb217aa4706c3ce06ce7232
Rails Udemy Course <railsudemycourse@gmail.com>
2019-08-23 22:03:07 +0100

    added category edits

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -17,7 +17,24 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "cat updated"
+      redirect_to category_path(@category)
+    else
+      flash[:danger] = "could not update"
+      render 'edit'
+    end
+  end
+
   def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
