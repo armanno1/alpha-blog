36cf117a12cfe34aef75883510020422be6301cc
Rails Udemy Course <railsudemycourse@gmail.com>
2019-08-23 19:34:46 +0100

    add article and cat ass

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @category = Category.create(name:"Sports")
+    @user = User.create(username: "John1234", email: "john@gmail.com", password: "password", admin: true)
   end
 
   test "should get categories index" do
@@ -12,6 +13,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    post login_path, params: { session: { email: "john@gmail.com", password: "password"} }
     get new_category_path
     assert_response :success
   end
@@ -19,6 +21,13 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     get category_path(@category)
     assert_response :success
+  end
+
+  test "should redirect if not admin" do
+    assert_no_difference "Category.count" do
+      post categories_path, params: { category: { name: "Another onerr" } }
+    end
+    assert_redirected_to categories_path
   end
 
 end
